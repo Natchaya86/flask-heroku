b104c15846799b4427f90653190c6b9107c27265
Natchaya86 <89177357+Natchaya86@users.noreply.github.com>
2021-09-02 11:07:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,10 @@ def hello():
 def get_api():
     return jsonify(data)
 
+@app.route('/hi')
+@app.route('/hello')
+def hi():
+    return "สวัสดีค่ะ"
+
 if __name__ == "__main__":
     app.run(debug=False)
